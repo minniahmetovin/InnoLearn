be3c30b907bbb429693990fd262deb5a4e04a527
Ильназ Нажипович Минниахметов <66380719+minniahmetovin@users.noreply.github.com>
2022-11-01 18:33:03 +0300

    Task03

--- src/task03/homework/multiplicationtable/MultiplicationTable.java
+++ src/task03/homework/multiplicationtable/MultiplicationTable.java
@@ -2,6 +2,13 @@ package task03.homework.multiplicationtable;
 
 public class MultiplicationTable {
     public static void main(String[] args) {
+        for (int t = 1; t < 10; t++) {
+            int x;
+            for (x = 1; x < 10; x++) {
+                System.out.print(t + "*" + x + "=" + t * x + " ");
+            }
+            System.out.println();
+        }
         // программа на вложенные циклы
     }
 }
--- src/task03/homework/progression/Progression.java
+++ src/task03/homework/progression/Progression.java
@@ -1,7 +1,22 @@
 package task03.homework.progression;
 
+import java.util.Scanner;
+
 public class Progression {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введи первое число");
+        int i = scanner.nextInt();
+        System.out.println("Введи знаменатель");
+        int a = scanner.nextInt();
+        int z = 1;
+        for (int y = 1; y < 11; y++) {
+//            for (int k = 0; k <= y - 1; k++) {
+//                z = z * a;
+//            }
+            System.out.println(i * (int)Math.pow(a,y-1));
+//            z = 1;
+        }
+
     }
 }
